ac78b4e34af64be555c66486fa3bdeb00bac44a5
Josh Madden <cyrex562@gmail.com>
2015-04-14 20:59:36 -0400

    rest api work

--- models.py
+++ models.py
@@ -4,108 +4,11 @@
 @author Josh Madden
 @copyright Fifth Column Group 2014
 """
-
-
-# class Voyage(db.Model):
-#     """
-#     Voyage DL object
-#     """
-#     voyage_id = db.Column(INTEGER, primary_key=True)
-#     voyage_name = db.Column(VARCHAR(255), index=True)
-#     voyage_notes = db.Column(TEXT)
-#     waypoints = db.relationship('Waypoint', lazy='dynamic')
-#     ships = db.relationship('Ship', lazy='dynamic')
-#
-#
-# class Ship(db.Model):
-#     """
-#     Ship DL object
-#     """
-#     ship_id = db.Column(INTEGER, primary_key=True)
-#     ship_name = db.Column(VARCHAR(255), index=True)
-#     ship_captain = db.Column(VARCHAR(255), index=True)
-#     ship_flag = db.Column(VARCHAR(255), index=True)
-#     ship_notes = db.Column(TEXT)
-#     ship_voyage_id = db.Column(db.Integer, db.ForeignKey('voyage.voyage_id'))
-#
-#
-# class Waypoint(db.Model):
-#     """
-#     Waypoint DL object
-#     """
-#     waypoint_id = db.Column(INTEGER, primary_key=True)
-#     waypoint_name = db.Column(VARCHAR(255), index=True)
-#     waypoint_type = db.Column(VARCHAR(255), index=True)
-#     waypoint_location = db.Column(VARCHAR(255), index=True)
-#     waypoint_notes = db.Column(TEXT)
-#     start_date = db.Column(TEXT, index=True)
-#     end_date = db.Column(TEXT, index=True)
-#     waypoint_voyage_id = db.Column(INTEGER, db.ForeignKey('voyage.voyage_id'))
-#     trades = db.relationship('Trade', lazy='dynamic')
-#
-#
-# class Trade(db.Model):
-#     """
-#     Trade DL object
-#     """
-#     trade_id = db.Column(INTEGER, primary_key=True)
-#     trade_notes = db.Column(TEXT)
-#     bought_sold = db.Column(BOOLEAN)
-#     trade_quantity = db.Column(INTEGER)
-#     trade_item = db.Column(TEXT)
-#     trade_waypoint_id = db.Column(db.Integer,
-#                                   db.ForeignKey('waypoint.waypoint_id'))
-#
-#
-# waypoint_types = [
-#     'start',
-#     'stop',
-#     'end'
-# ]
-#
-#
-# trade_items = \
-#     [
-#         "male slave",
-#         "female slave",
-#         "child slave",
-#         "guns",
-#         "cookware",
-#         "livestock",
-#         "wild game",
-#         "fish",
-#         "seafood",
-#         "foodstuffs",
-#         "grain",
-#         "timber",
-#         "minerals",
-#         "metals",
-#         "tea",
-#         "coffee",
-#         "tobacco",
-#         "cocoa",
-#         "sugar",
-#         "spices",
-#         "alcohol",
-#         "textiles",
-#         "medicine",
-#         "jewelry",
-#         "furs",
-#         "leather",
-#         "paper",
-#         "manufactured goods",
-#         "luxury items",
-#         "dyes",
-#         "plants",
-#         "cotton",
-#         "wool",
-#         "other"
-#     ]
-
 mongo = None
+app = None
 
 
-def init(in_mongo):
+def init(app, in_mongo):
     global mongo
     mongo = in_mongo
 
@@ -114,22 +17,22 @@ def get_all_voyages():
     return list(mongo.db.voyages.find())
 
 
-def add_voyage(new_voyage):
-    add_voyage_result = mongo.db.voyages.insert(new_voyage)
+def add_voyage(voyage_to_add):
+    add_voyage_result = mongo.db.voyages.insert(voyage_to_add)
     print "add_voyage_result: {0}".format(add_voyage_result)
     return add_voyage_result
 
 
-def modify_voyage(mod_voyage):
-    modify_voyage_result = mongo.db.voyages.update({"voyage_id": mod_voyage[
-        "voyage_id"]}, mod_voyage)
+def modify_voyage(voyage_to_modify):
+    modify_voyage_result = mongo.db.voyages.update(
+        {"voyage_id": voyage_to_modify["voyage_id"]}, voyage_to_modify)
     print "modify_voyage_result: {0}".format(modify_voyage_result)
     return modify_voyage_result
 
 
-def delete_voyage(del_voyage):
+def delete_voyage(voyage_to_delete):
     del_voyage_result = mongo.db.voyages.remove(
-        {"voyage_id": del_voyage["voyage_id"]})
+        {"voyage_id": voyage_to_delete["voyage_id"]})
     print "del_voyage_result: {0}".format(del_voyage_result)
     return del_voyage_result
 
@@ -140,15 +43,78 @@ def get_voyage_by_id(voyage_id):
     return find_voyage_result
 
 
-def stringify_object_ids(voyage_list):
-    new_voyages_list = []
-    for v in voyage_list:
-        v['_id'] = str(v['_id'])
-        new_voyages_list.append(v)
-    return new_voyages_list
+def get_voyages_by_id(voyage_ids):
+    app.logger.debug('get_voyages_by_ids: NOT IMPLEMENTED')
+    pass
 
 
+def stringify_object_ids(in_list):
+    new_list = []
+    for i in in_list:
+        i['_id'] = str(i['_id'])
+        new_list.append(i)
+    return new_list
 
 
+def add_waypoint(waypoint_to_add):
+    add_waypoint_result = mongo.db.waypoints.insert(waypoint_to_add)
+    app.logger.debug('add waypoint result: {0}'.format(add_waypoint_result))
+    return add_waypoint_result
+
+
+def modify_waypoint(waypoint_to_modify):
+    modify_waypoint_result = mongo.db.waypoints.update(
+        {"waypoint_id": waypoint_to_modify["waypoint_id"]}, waypoint_to_modify)
+    app.logger.debug(
+        "modify waypoint result: {0}".format(modify_waypoint_result))
+    return modify_waypoint_result
+
+
+def delete_waypoint(waypoint_to_delete):
+    delete_waypoint_result = mongo.db.waypoints.remove(
+        {"waypoint_id": waypoint_to_delete["waypoint_id"]})
+    app.logger.debug(
+        "delete waypoint result: {0}".format(delete_waypoint_result))
+    return delete_waypoint_result
+
+
+def get_all_waypoints():
+    return list(mongo.db.waypoints.find())
+
+
+def get_waypoint_by_id(waypoint_id):
+    find_waypoint_result = mongo.db.waypoints.find_one(
+        {"waypoint_id": waypoint_id})
+    app.logger.debug("get_waypoint_id: find_waypoint_result: {0}".format(
+        find_waypoint_result))
+    return find_waypoint_result
+
+
+def get_waypoints_by_id(waypoint_ids):
+    pass
+
+
+def get_all_trades():
+    pass
+
+
+def get_trade_by_id(trade_id):
+    pass
+
+
+def get_trades_by_id(trade_ids):
+    pass
+
+
+def add_trade(trade_to_add):
+    pass
+
+
+def modify_trade(trade_to_modify):
+    pass
+
+
+def delete_trade(trade_to_delete):
+    pass
 
 
--- views.py
+++ views.py
@@ -23,11 +23,6 @@ models.init(mongo)
 
 
 def set_session_id(in_session):
-    """
-
-    :param in_session:
-    :return:
-    """
     if 'session_id' in in_session:
         result = in_session['session_id']
     else:
@@ -38,28 +33,16 @@ def set_session_id(in_session):
 
 @app.route('/')
 def default_page_controller():
-    """
-    default route handler
-    :return:
-    """
     return redirect(url_for('voyages_page_get'))
 
 
 @app.route('/voyages', methods=['GET'])
 def voyages_page_get():
-    """
-    page controller
-    :return:
-    """
     return render_template('voyages2.html')
 
 
 @app.route('/voyages/get', methods=['POST'])
 def voyages_get():
-    """
-
-    :return:
-    """
     app.logger.debug('voyages_get')
     voyage_ids = request.json["params"]["voyage_ids"]
 
@@ -78,31 +61,119 @@ def voyages_get():
 
 
 @app.route('/voyages/add', methods=['POST'])
-def voyages_post():
-    """
-
-    :return:
-    """
+def voyages_add():
     app.logger.debug('voyages_post')
-    new_voyage = request.json["params"]["new_voyage"]
+    new_voyage = request.json["params"]["voyage_to_add"]
+    new_voyage['waypoints'] = \
+        models.get_waypoints_by_id(new_voyage['waypoints'])
     add_result = models.add_voyage(new_voyage)
-    return jsonify(message="success", data="")
+    return jsonify(message="success", data={"added_voyage_id": add_result})
 
 
 @app.route('/voyages/modify', methods=['POST'])
-def voyages_put():
+def voyages_modify():
     app.logger.debug('voyages_post')
-    mod_voyage = request.json["params"]["modified_voyage"]
+    mod_voyage = request.json["params"]["voyage_to_modify"]
     mod_result = models.modify_voyage(mod_voyage)
-    return jsonify(message="success", data="")
+    return jsonify(message="success", data={"modified_voyage_id": mod_result})
 
 
 @app.route('/voyages/delete', methods=['POST'])
 def voyages_delete():
     app.logger.debug('voyages_delete')
-    del_voyage = request.json["params"]["del_voyage"]
+    del_voyage = request.json["params"]["voyage_to_delete"]
     del_result = models.delete_voyage(del_voyage)
-    return jsonify(message="success", data="")
+    return jsonify(message="success", data={"deleted_voyage_id": del_result})
+
+
+@app.route('/waypoints/get', methods=['POST'])
+def waypoints_get():
+    app.logger.debug('waypoint_get')
+    waypoint_ids = request.json['params']['waypoint_ids']
+    if len(waypoint_ids) == 0:
+        waypoints = models.get_all_waypoints()
+        waypoints = models.stringify_object_ids(waypoints)
+    elif len(waypoint_ids) == 1:
+        waypoint = models.get_waypoint_by_id(waypoint_ids[0])
+        waypoints = [waypoint]
+        waypoints = models.stringify_object_ids(waypoints)
+    else:
+        waypoints = []
+        app.logger.error('waypoints_get: NOT IMPLEMENTED')
+    result = jsonify(message="success", data={"waypoints": waypoints})
+    return result
+
+
+@app.route('/waypoints/add', methods=['POST'])
+def waypoints_add():
+    app.logger.debug('waypoints_add')
+    waypoint_to_add = request.json["params"]["waypoint_to_add"]
+    add_waypoint_result = models.add_waypoint(waypoint_to_add)
+    return jsonify(message="success",
+                   data={"added_waypoint_id": add_waypoint_result})
+
+
+@app.route('/waypoints/modify', methods=['POST'])
+def waypoints_modify():
+    app.logger.debug('waypoints_modify')
+    waypoint_to_modify = request.json["params"]["waypoint_to_modify"]
+    modify_waypoint_result = models.modify_waypoint(waypoint_to_modify)
+    return jsonify(message="success",
+                   data={"modified_waypoint_id": modify_waypoint_result})
+
+
+@app.route('/waypoints/delete', methods=['POST'])
+def waypoints_delete():
+    app.logger.debug('waypoints_delete')
+    waypoint_to_delete = request.json["params"]["waypoint_to_delete"]
+    delete_waypoint_result = models.delete_waypoint(waypoint_to_delete)
+    return jsonify(message="success",
+                   data={"deleted_waypoint_id": delete_waypoint_result})
+
+
+@app.route('/trades/get', methods=['POST'])
+def trades_get():
+    app.logger.debug('trades_get')
+    trade_ids = request.json['params']['trade_ids']
+    if len(trade_ids) == 0:
+        trades = models.get_all_trades()
+        trades = models.stringify_object_ids(trades)
+    elif len(trade_ids) == 1:
+        trade = models.get_trade_by_id(trade_ids[0])
+        trades = [trade]
+        trades = models.stringify_object_ids(trades)
+    else:
+        trades = []
+        app.logger.error('trades_get: NOT IMPLEMENTED')
+    result = jsonify(message='success', data={"trades": trades})
+    return result
+
+
+@app.route('/trades/add', methods=['POST'])
+def trades_add():
+    app.logger.debug('trades_add')
+    trade_to_add = request.json["parms"]["trade_to_add"]
+    add_trade_result = models.add_trade(trade_to_add)
+    return jsonify(message="success",
+                   data={"added_trade_id": add_trade_result})
+
+
+@app.route('/trades/modify', methods=['POST'])
+def trades_modify():
+    app.logger.debug('trades_modify')
+    trade_to_modify = request.json["params"]["trade_to_modify"]
+    modify_trade_result = models.modify_trade(trade_to_modify)
+    return jsonify(message="success",
+                   data={"modified_trade_id": modify_trade_result})
+
+
+@app.route('/trades/delete', methods=['POST'])
+def trades_delete():
+    app.logger.debug('trades_delete')
+    trade_to_delete = request.json["params"]["trade_to_delete"]
+    delete_trade_result = models.delete_trade(trade_to_delete)
+    return jsonify(message="success",
+                   data={"deleted_trade_id": delete_trade_result})
 
 
 if __name__ == '__main__':
